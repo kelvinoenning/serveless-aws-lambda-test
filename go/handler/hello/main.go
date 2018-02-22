9d58ae09d896320ba3cc5de6966339eceec2ccad
Kelvin Stang Oenning <kelvin@MacBook-Pro-de-Kelvin.local>
2018-02-22 13:31:13 -0300

    create sls functions

--- go/handler/hello/main.go
+++ go/handler/hello/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Received body: ", request.Body)
-
-	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{Body: ":)", StatusCode: 200}, nil
 }
 
 func main() {
